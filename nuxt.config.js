31882f94e4e0bbdd8fc13cfb963e2b9c82b614e6
lilstar <lilstar.shin@gmail.com>
2022-06-03 16:22:42 +0900

    init

---
+++ nuxt.config.js
@@ -0,0 +1,26 @@
+export default {
+  ssr: false,
+
+  target: "static",
+
+  head: {
+    title: "mbti-test",
+    htmlAttrs: {
+      lang: "en",
+    },
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+  },
+  css: ["~/assets/css/global.css"],
+  plugins: [],
+  components: true,
+  buildModules: [],
+
+  modules: [],
+  build: {},
+};
